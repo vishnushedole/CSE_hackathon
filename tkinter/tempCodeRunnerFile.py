f7ccb705beb9ddcc7d738e7bd7efefeece4eea5f
Chinmay G Degaonkar <83526406+chinmaygd02@users.noreply.github.com>
2022-11-25 10:44:58 +0530

    Add files via upload

---
+++ tkinter/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+body_text_line1 = Label(frame,text="Malware, or malicious software, is any program or file that is intentionally",font=courier15,fg="orange",bg="black" )
+# body_text_line2 = Label(frame,text="harmful to a computer, network or server.Types of malware include computer ",fg="orange",font=courier15,bg="black")
+# body_text_line3 = Label(frame,text="viruses, worms, Trojan horses, ransomware and spyware.Detect your malware ",font=courier15,bg="black",fg="orange")
+# body_text_line4 = Label(frame,text="with our toolfor free.Get rid of viruses and keep your computer secure. ",font=courier15,bg="black",fg="orange")
+# body_text_line1.place(x=10,y=100)
+# body_text_line2.place(x=10,y=130)
+# body_text_line3.place(x=10,y=160)
+# body_text_line4.place(x=10,y=190)
